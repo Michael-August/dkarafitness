78392d7610b2d79414ea902cc3f37a09f2aebea3
Michael-August <michaelfrancis135@gmail.com>
2024-05-22 10:50:11 +0100

    TF5 as prefix for code

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -63,6 +63,7 @@ export function PaymentForm() {
                 handleGenerateCode()
                 setFormData({})
             }
+            handleGenerateCode()
         }
     }
 
--- src/utils/codeGenerator.js
+++ src/utils/codeGenerator.js
@@ -5,15 +5,10 @@ export const generateCode = () => {
 	const getRandomChar = (chars) =>
 		chars.charAt(Math.floor(Math.random() * chars.length));
 
-	let newCode = "";
-
-	// Add two random uppercase letters
-	for (let i = 0; i < 2; i++) {
-		newCode += getRandomChar(letters);
-	}
+	let newCode = "TF5";
 
 	// Add four random digits
-	for (let i = 0; i < 4; i++) {
+	for (let i = 0; i < 3; i++) {
 		newCode += getRandomChar(digits);
 	}
 
